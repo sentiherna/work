eb4786c5b53eb808271c98474def4afef3a3c357
sentiherna <153864974+sentiherna@users.noreply.github.com>
2023-12-14 20:24:14 -0300

    Add files via upload

---
+++ aws/emr-ba.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo pip-3.6 install awswrangler==1.1.0
+
+#sudo mkdir /usr/share/databricks
+#sudo wget -P /usr/share/databricks/ https://s3-us-west-2.amazonaws.com/awslabs-mx-bdws/lab07/prework/assets/databricks/latest/spark-redshift_2.10-2.0.1.jar
+#sudo wget -P /usr/share/databricks/ https://s3-us-west-2.amazonaws.com/awslabs-mx-bdws/lab07/prework/assets/databricks/latest/spark-redshift_2.11-2.0.1.jar
+#sudo wget -P /usr/share/databricks/ https://s3-us-west-2.amazonaws.com/awslabs-mx-bdws/lab07/prework/assets/databricks/latest/spark-avro_2.11-4.0.0.jar
